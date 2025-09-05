63d66890ceb062dd12fe584a852b568b08a9440b
Collins Kipruto <collinskipruto036@gmail.com>
2025-09-05 23:37:27 +0300

    submit

---
+++ prodev-mobile-app-0x06/app/index.tsx
@@ -0,0 +1,117 @@
+import { Link, useRouter } from "expo-router";
+import { Text, View, ImageBackground, StyleSheet, Dimensions, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+const styles = StyleSheet.create({
+container:{
+flex:1
+},
+
+background:{
+  flex:1,
+  justifyContent: "center",
+  height: Dimensions.get("window").height, //covers the entire screen
+  width: Dimensions.get("window").width //covers the entire screen
+},
+
+companyLogo:{
+  width: "100%",
+  alignItems:"center",
+  padding:20,
+  marginBottom:50
+},
+
+textGroup:{
+  alignItems: "center"
+},
+textLarge:{
+  color: "white",
+  fontSize: 40,
+  fontWeight:"200",
+  textAlign:"center",
+  marginBottom: 12
+  
+},
+
+textSmall:{
+color:"white",
+fontSize: 18,
+fontWeight: "200",
+textAlign: "center"
+
+},
+
+transparentButton:{
+borderColor: "white",
+borderWidth: 2,
+borderRadius: 40,
+paddingVertical: 15,
+paddingHorizontal: 5,
+alignItems: "center",
+fontSize: 20,
+flex:1,
+},
+
+button:{
+borderColor: "white",
+borderWidth: 2,
+borderRadius: 40,
+paddingVertical: 15,
+paddingHorizontal: 5,
+alignItems: "center",
+fontSize: 20,
+backgroundColor: "white",
+flex: 1,
+},
+
+buttonGroup:{
+ flexDirection: "row",
+    gap: 20,
+    paddingHorizontal: 20,
+  },
+})
+
+export default function Index() {
+
+const router = useRouter()
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex:1}}>
+        <ImageBackground source={require("@/assets/images/background-image.png")} style={styles.background} resizeMode="cover">
+        {/*Other Components go here */}
+       <View style={styles.companyLogo}>
+        <Image source={require("@/assets/images/logo.png")} style={{height:100 ,width:100}}></Image>
+       </View>
+       <View style={styles.textGroup}>
+        <Text style={styles.textLarge}>Find your favorite place here</Text>
+        <Text style={styles.textSmall}>The best prices for over 2 </Text>
+        <Text style={styles.textSmall}>million properties worldwide</Text>
+       </View>
+       <View style={styles.buttonGroup}>
+        <TouchableOpacity style={styles.button} onPress={()=>{router.push("/join")}}>
+          <Text style={{...styles.textSmall,color:"black"}}>Join here</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.transparentButton}>
+         <Link href="/signin"  push><Text style={styles.textSmall}>Sign In</Text></Link> 
+        </TouchableOpacity>
+
+
+
+       </View>
+       <View style={{alignItems:"center", paddingVertical:20}}>
+        <Text style={{color:"white"}}>Continue to home</Text>
+       </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
+    </View>
+  );
+}
---
+++ prodev-mobile-app-0x06/app/join.tsx
@@ -0,0 +1,53 @@
+import { styles } from "@/styles/_join";
+import { Text, TextInput, View, TouchableOpacity, SafeAreaView} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
+
+export default function Join(){
+      return (
+          <SafeAreaProvider>
+            <SafeAreaView>
+                <View style={styles.container}>
+                 <MaterialIcons name="arrow-back" size={25}/>
+                </View>
+                 <Text style={styles.titleTextGroup}></Text> 
+                 <Text style={styles.titleText}></Text>
+                 <Text style={styles.subText}></Text>
+            
+            <View style={styles.formGroup}>
+              <Text style={styles.formLabel}></Text>
+              <View style={styles.formControl}>
+              <View style={styles.formPasswordControl}>
+                <TextInput style={styles.passwordControl} />
+              </View>
+              </View> 
+              </View>
+              <Text style={styles.forgotPasswordText}></Text>
+            <TouchableOpacity style={styles.primaryButton}>
+              <Text style={styles.buttonText}></Text>
+            </TouchableOpacity>
+
+           <View style={styles.dividerGroup}>
+            <View style={styles.divider}>
+              <Text style={styles.dividerText}></Text>
+            </View>
+           </View>
+
+            <View style={styles.secondaryButtonGroup}> 
+            <TouchableOpacity style={styles.secondaryButton}>
+              <Text style={styles.secondaryButtonText}></Text>
+            </TouchableOpacity>
+            </View>
+
+            <View style={styles.signupgroup}>
+              <Text style={styles.signupTitleText}></Text>
+              <Text style={styles.signupSubTitleText}></Text>
+            </View>
+
+            </SafeAreaView>
+          </SafeAreaProvider>
+      );
+}
